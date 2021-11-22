7926944273a4ab3c8e89fc158ec0873bc97e8e77
Eber de Oliveira Costa <eber.oliv.costa@gmail.com>
2021-11-22 16:32:03 -0300

    Criação da Tela de Login

---
+++ app/src/main/java/com/eber/projetooficial/FrmLogin.java
@@ -0,0 +1,14 @@
+package com.eber.projetooficial;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class FrmLogin extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_frm_login);
+    }
+}
