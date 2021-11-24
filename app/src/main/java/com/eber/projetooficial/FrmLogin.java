076628d2f8101c2b03c696128aefbc125ab32305
Eber de Oliveira Costa <eber.oliv.costa@gmail.com>
2021-11-24 11:51:31 -0300

    Configurado estilos da Tela de Cadastro.

--- app/src/main/java/com/eber/projetooficial/FrmLogin.java
+++ app/src/main/java/com/eber/projetooficial/FrmLogin.java
@@ -2,9 +2,14 @@ package com.eber.projetooficial;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
 
 public class FrmLogin extends AppCompatActivity {
+
+    private TextView text_tela_cadastro;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -13,6 +18,20 @@ public class FrmLogin extends AppCompatActivity {
 
         getSupportActionBar().hide();
 
+        IniciarComponentes();
 
+        text_tela_cadastro.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(FrmLogin.this, Frm_Cadastro.class);
+                startActivity(intent);
+
+            }
+        });
+
+    }
+
+    private void IniciarComponentes(){
+        text_tela_cadastro = findViewById(R.id.text_tela_cadastro);
     }
 }
--- app/src/main/java/com/eber/projetooficial/Frm_Cadastro.java
+++ app/src/main/java/com/eber/projetooficial/Frm_Cadastro.java
@@ -4,11 +4,16 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 
-public class frm_Cadastro extends AppCompatActivity {
+public class Frm_Cadastro extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_frm_cadastro);
+
+        getSupportActionBar().hide();
+
+
     }
+
 }
