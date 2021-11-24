547da7972b3d0de494dedcb347718ba6085f5aa8
Eber de Oliveira Costa <eber.oliv.costa@gmail.com>
2021-11-24 10:04:42 -0300

    Adicionado componentes responsivos da Tela de Login. Criado Activity da Tela de Cadastro.

--- app/src/main/java/com/eber/projetooficial/frm_Cadastro.java
+++ app/src/main/java/com/eber/projetooficial/frm_Cadastro.java
@@ -4,15 +4,11 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 
-public class FrmLogin extends AppCompatActivity {
+public class frm_Cadastro extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_frm_login);
-
-        getSupportActionBar().hide();
-
-
+        setContentView(R.layout.activity_frm_cadastro);
     }
 }
