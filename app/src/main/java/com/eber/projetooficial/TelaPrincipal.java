9cd869eabdb29ef19f57735557aaeb6b777a9d88
Eber de Oliveira Costa <eber.oliv.costa@gmail.com>
2021-11-24 16:36:24 -0300

    Criado estrutura da Tela Principal.

--- app/src/main/java/com/eber/projetooficial/TelaPrincipal.java
+++ app/src/main/java/com/eber/projetooficial/TelaPrincipal.java
@@ -4,12 +4,12 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 
-public class Frm_Cadastro extends AppCompatActivity {
+public class TelaPrincipal extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_frm_cadastro);
+        setContentView(R.layout.activity_tela_principal);
 
         getSupportActionBar().hide();
 
